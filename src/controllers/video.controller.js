15cf6c121d72e6fe3b5829020ead219a9017da99
Nitish Choudhary <nitish.chy.007@gmail.com>
2024-08-31 13:17:44 +0530

    video controller: added additional functions

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,6 +8,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+  console.log(query, sortBy, sortType, userId);
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -53,16 +54,88 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+
+  // CHECKING IF vedioID IS PRESENT AND IS VALID
+  if (!videoId || videoId.trim() === "") {
+    throw new ApiError(400, "No videoId was provided");
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  // FINDING AND RETURNING THE VIDEO
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video found successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+
+  // CHECKING IF vedioID IS PRESENT AND IS VALID
+  if (!videoId || videoId.trim() === "") {
+    throw new ApiError(400, "No videoId was provided");
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  const fieldsToUpdate = {};
+  if (req.body?.title) fieldsToUpdate.title = req.body.title;
+  if (req.body?.description) fieldsToUpdate.description = req.body.description;
+  if (req.file) {
+    const thumbnailLocalPath = req.file?.path;
+    const thumbnailUploadResponse =
+      await uploadOnCloudinary(thumbnailLocalPath);
+    if (thumbnailUploadResponse)
+      fieldsToUpdate.thumbnail = thumbnailUploadResponse.url;
+    else throw new ApiError(400, "Thumbnail update failed");
+  }
+
+  if (Object.keys(fieldsToUpdate).length === 0) {
+    throw new ApiError(400, "Atleast one fieldto update is required");
+  }
+  const updatedVideo = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: fieldsToUpdate,
+    },
+    { new: true, runValidators: false }
+  );
+  if (!updatedVideo) throw new ApiError(404, "Video not found");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedVideo, "Successfully updated video"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: delete video
+
+  // CHECKING IF vedioID IS PRESENT AND IS VALID
+  if (!videoId || videoId.trim() === "") {
+    throw new ApiError(400, "No videoId was provided");
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  const deletedVideo = await Video.findByIdAndDelete(videoId);
+  if (!deletedVideo) {
+    throw new ApiError(404, "Video not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
